c769620ef785410e4f2598b4638526f10717314d
Corinna Kelly <corinna1@UAlberta.ca>
2020-10-05 10:59:10 -0600

    Add project files.

---
+++ capstone-project-team-coco/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace capstone_project_team_coco.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
