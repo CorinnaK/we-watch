2312c2c5c68719b401b0649b588146b24ac7cc4a
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-10-06 14:50:16 -0600

    Moved ForeignKey properties to the correct areas in the code above the appropriate columns

--- capstone-project-team-coco/Models/Watch_History.cs
+++ capstone-project-team-coco/Models/Watch_History.cs
@@ -20,43 +20,35 @@ namespace we_watch.Models
         [Required]
         public DateTime ID { get; set; }
 
+        [ForeignKey(nameof(Season_Num))]
         [Column("Season_Num", TypeName = "smallint(2)")]
         [Required]
         public int Season_Num { get; set; }
 
+        [ForeignKey(nameof(Episode_Num))]
         [Column("Episode_Num", TypeName = "smallint(2)")]
         [Required]
         public int Episode_Num { get; set; }
 
+        [ForeignKey(nameof(Show_ID))]
         [Column("Show_ID", TypeName = "varchar(50)")]
         [Required]
         public int Show_ID { get; set; }
 
+        [ForeignKey(nameof(Watcher_ID))]
         [Column("Watcher_Id", TypeName = "int(10)")]
         [Required]
         public int Watcher_ID { get; set; }
 
+        [ForeignKey(nameof(Platform))]
         [Column("Platform", TypeName = "varchar(20)")]       
         public string Platform { get; set; }
 
        
         [InverseProperty(nameof(Models.Show_Card.Watch_History))]
+                
+        
         public virtual ICollection<Show_Card> Show_Cards { get; set; } // Watcher_History can have a collection of multiple show cards (one watcher history to many show cards)
-
-        [ForeignKey(nameof(Season_Num))]
-
-        [ForeignKey(nameof(Episode_Num))]
-
-        [ForeignKey(nameof(Show_ID))]
-        
-        [ForeignKey(nameof(Watcher_ID))]
-        
-        [ForeignKey(nameof(Platform))]
-
- 
-
-
-
 
 
     }
