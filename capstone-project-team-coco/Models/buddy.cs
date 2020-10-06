322839d57bfbe034d9822be7fbda8bbfb1139599
Corinna Kelly <corinna1@UAlberta.ca>
2020-10-06 12:34:59 -0600

    Added buddy and show classes. Changed namespace to we_watch

---
+++ capstone-project-team-coco/Models/buddy.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace we_watch.Models
+{
+    [Table("buddy")]
+    public partial class Buddy
+    {
+
+        [Key]
+        [Column("ID", TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int ID { get; set; }
+
+
+        [Column(TypeName = "varchar(30)")]
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
---
+++ capstone-project-team-coco/Models/show.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace we_watch.Models
+{
+    [Table("show")]
+    public partial class Show
+    {
+
+        [Key]
+        [Column("ID", TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int ID { get; set; }
+
+    
+        [Column(TypeName = "varchar(50)")]
+        [Required]
+        public string Title { get; set; }
+
+
+        [Column(TypeName = "smallint(2)")]
+        [Required]
+        public string Total_Seasons { get; set; }
+
+    }
+}
