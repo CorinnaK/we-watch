07443a50781cf191f698fd159ec392f3a83bb0e8
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-10-09 13:06:25 -0600

    created if statements to compare user inputs on email, confirmedemail, password, confirmedpassword; added viewbags to SignUp.cshtml

--- capstone-project-team-coco/Controllers/UserController.cs
+++ capstone-project-team-coco/Controllers/UserController.cs
@@ -51,44 +51,78 @@ namespace we_watch.Controllers
 
         public IActionResult SignUp()
         {
-            using (WeWatchContext context = new WeWatchContext())
-            {
-                int count = context.User.Count();
-                ViewBag.Count = count;
-
-            }
+            // needed to show our form
             return View();
         }
-        [HttpPost]
-        public IActionResult SignUp(string email, string password)
+
+        [HttpPost] //method runs on submit
+        public IActionResult SignUp(string email, string confirmedemail, string password, string confirmedpassword)
         {
-            if (ModelState.IsValid)
+            if (email == null)
             {
-                using (WeWatchContext context = new WeWatchContext())
-                {
-                    if (context.User.Where(x => x.Email == email).Count() != 1)
-                    {
-                        ViewBag.Email = "An incorrect Email and/or Password";
-                        ViewBag.Password = "Please Try Again";
-                    }
-                    else
-                    {
-                        ViewBag.Email = email;
-                        ViewBag.Password = password;
-
-                    }
-
-                }
+                ViewBag.email = "Please enter an email address.";
             }
+
+            else if (confirmedemail == null)
+            {
+                ViewBag.confirmedemail = "Please confirm your email.";
+            }
+
+            else if (email != confirmedemail)
+            {
+                ViewBag.matchingemail = "These emails do not match. Please try again.";
+            }
+
+            else if(password == null)
+            {
+                ViewBag.password = "Please enter a password.";
+            }
+
+            else if(confirmedpassword == null)
+            {
+                ViewBag.confirmedemail = "Please confirm your password.";
+            }
+
+            else if (password != confirmedpassword)
+            {
+                ViewBag.confirmedpassword = "These passwords do not match. Please try again.";
+            }
+
+            return View();
+        }
+            
+
+
+            /* if (ViewBag.email != ViewBag.confirmedemail)
+             {
+
+
+
+                 using (WeWatchContext context = new WeWatchContext())
+                 {
+                     if (context.User.Where(x => x.Email == email).Count() != 1)
+                     {
+                         ViewBag.Email = "An incorrect Email and/or Password";
+                         ViewBag.Password = "Please Try Again";
+                     }
+                     else
+                     {
+                         ViewBag.Email = email;
+                         ViewBag.Password = password;
+
+                     }*/
+
+        //}
+           // }
 
 
             // default view
-            return View();
+           // return View();
         }
 
 
     }
-}
+
 
 
 
