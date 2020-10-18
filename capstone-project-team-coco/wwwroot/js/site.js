03a17dcb58e6e95271e4dddc38acec4cc7164ec7
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-10-18 14:16:14 -0600

    Created drop down functionality & script on MangeShows page

--- capstone-project-team-coco/wwwroot/js/site.js
+++ capstone-project-team-coco/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/*<script>
+    var coll = document.getElementsByClassName("programTitle");
+    let i;
+
+for (i = 0; i < coll.length; {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+
+            let content = this.parentNode.nextElementSibling;
+            content.classList.toggle("expanded");
+</script>*/
+
+/*<script type="text/javascript" language="javascript">
+    function onSubmitButtonClick() {
+        alert("submit button clicked");
+ }
+</script>*/
+
+/*let programTitle = document.getElementsByClassName("programTitle");
+let i;
+
+for (i = 0; i < programTitle.length; i++) {
+    programTitle[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        let content = this.parentNode.nextElementSibling;
+        content.classList.toggle("expanded");
+    });
+}*/
