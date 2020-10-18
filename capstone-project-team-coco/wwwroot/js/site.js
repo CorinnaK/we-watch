5fb7b0d3b0cf10867d672624d985caa54346f2ff
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-10-18 14:45:50 -0600

    Updated link to site.js from ManageShows.cshtml

--- capstone-project-team-coco/wwwroot/js/site.js
+++ capstone-project-team-coco/wwwroot/js/site.js
@@ -2,32 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-/*<script>
-    var coll = document.getElementsByClassName("programTitle");
-    let i;
 
-for (i = 0; i < coll.length; {
-        coll[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-
-            let content = this.parentNode.nextElementSibling;
-            content.classList.toggle("expanded");
-</script>*/
-
-/*<script type="text/javascript" language="javascript">
-    function onSubmitButtonClick() {
-        alert("submit button clicked");
- }
-</script>*/
-
-/*let programTitle = document.getElementsByClassName("programTitle");
+let programTitle = document.getElementsByClassName("programTitle");
 let i;
 
 for (i = 0; i < programTitle.length; i++) {
     programTitle[i].addEventListener("click", function () {
         this.classList.toggle("active");
 
-        let content = this.parentNode.nextElementSibling;
-        content.classList.toggle("expanded");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
     });
-}*/
+}
