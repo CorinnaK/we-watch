e5a4cc798b250a3baf58bf98a6fc8c50cb192e45
Corinna Kelly <corinna1@UAlberta.ca>
2020-10-20 09:13:14 -0600

    Cleaned up code

--- capstone-project-team-coco/Models/DisplayCard.cs
+++ capstone-project-team-coco/Models/DisplayCard.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.EntityFrameworkCore.Storage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace we_watch.Models
 {
@@ -18,12 +13,10 @@ namespace we_watch.Models
         public int SeasonID { get => _seasonID;
             set
             {
-                using (WeWatchContext context = new WeWatchContext())
-                {
-                    _seasonID = value;
-                    CurrentSeason = context.ShowSeason.Where(x => x.ShowSeasonID == value).Select(x => x.IndividualSeason).Single();
-                    Episodes = context.ShowSeason.Where(x => x.ShowSeasonID == value).Select(x => x.SeasonEpisodes).Single();
-                }
+                using WeWatchContext context = new WeWatchContext();
+                _seasonID = value;
+                CurrentSeason = context.ShowSeason.Where(x => x.ShowSeasonID == value).Select(x => x.IndividualSeason).Single();
+                Episodes = context.ShowSeason.Where(x => x.ShowSeasonID == value).Select(x => x.SeasonEpisodes).Single();
             }
 
         }
@@ -35,13 +28,11 @@ namespace we_watch.Models
 
         public DisplayCard(int showID, int watcherID)
         {
-            using (WeWatchContext context = new WeWatchContext())
-            {
-                ShowID = showID;
-                WatcherID = watcherID;
-                ShowTitle = context.Show.Where(x => x.ShowID == showID).Select(x => x.Title).Single().ToString();
-                WatcherName = context.Watcher.Where(x => x.WatcherID == watcherID).Select(x => x.Name).Single().ToString();
-            }
+            using WeWatchContext context = new WeWatchContext();
+            ShowID = showID;
+            WatcherID = watcherID;
+            ShowTitle = context.Show.Where(x => x.ShowID == showID).Select(x => x.Title).Single().ToString();
+            WatcherName = context.Watcher.Where(x => x.WatcherID == watcherID).Select(x => x.Name).Single().ToString();
         }
 
     }
